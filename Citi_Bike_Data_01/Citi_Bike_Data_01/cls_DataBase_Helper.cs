1e772185eff88a0d5cad7007613ec64fa6be5e8d
CharlieP <charlie@flux.io>
2018-04-06 14:52:35 -0400

    once more

---
+++ Citi_Bike_Data_01/Citi_Bike_Data_01/cls_DataBase_Helper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
+
+namespace Citi_Bike_Data_01
+{
+    public class cls_DataBase_Helper
+    {
+        /// <summary>
+        /// Get the list of table names be retrieving the FileNames column in the first table
+        /// </summary>
+        /// <param name="window"> Main dilaog window </param>
+        /// <param name="connectionString"> Connection string to the database </param>
+        /// <returns> List of strings containing the table names </returns>
+        public static List<string> GetTableNames(MainWindow window, string connectionString)
+        {
+            List<string> tableNames = new List<string>();                                       // empty list for the names
+
+            using (SqlConnection connection = new SqlConnection(connectionString))              // create connection to DB
+            {
+                string SqlQuery = "SELECT FileName FROM FileNames";                             // DB query
+                SqlCommand cmd = new SqlCommand(SqlQuery, connection);                          // create command
+                connection.Open();                                                              // open connection to DB
+                SqlDataReader reader = cmd.ExecuteReader();                                     // execute read command
+                while (reader.Read())
+                {
+                    tableNames.Add(reader["FileName"].ToString());                              // read all the rows
+                }
+                connection.Close();                                                             // close connection
+            }   
+            return tableNames;                                                                  // return list of names
+        }
+
+
+        /// <summary>
+        /// Get the list of table names by retrieving the name of each table
+        /// </summary>
+        /// <param name="connectionString"> Connection string ot the database </param>
+        /// <param name="window"> Main dialog window </param>
+        /// <returns> List of strings containing the table names </returns>
+        public static List<string> GetTableNames(string connectionString, MainWindow window)
+        {
+            List<string> tableNames = new List<string>();                                       // empty list for the names
+
+            using (SqlConnection connection = new SqlConnection(connectionString))              // create connection to DB
+            {
+                connection.Open();                                                              // open connection to DB
+                DataTable table = connection.GetSchema("Tables");                               // get the table
+                connection.Close();                                                             // close connection
+
+                foreach(DataRow r in table.Rows)
+                {
+                    tableNames.Add(r[2].ToString());                                            // save each table name
+                }
+            }
+            return tableNames;                                                                  // return list of names
+        }
+        
+
+        /// <summary>
+        /// Adds the names of the new datasets to the first table in the DB
+        /// </summary>
+        /// <reference>Does table exist:https://stackoverflow.com/questions/2923452/c-sharp-sql-create-table-if-it-doesnt-already-exist </reference>
+        /// <reference>Create connection to database: https://stackoverflow.com/questions/12220865/connecting-to-local-sql-server-database-using-c-sharp </reference>
+        /// <reference>https://stackoverflow.com/questions/6671067/retrieve-list-of-tables-from-specific-database-on-server-c-sharp</reference>
+        public static void AddFileNamesToTable(string connectionString, List<string> FileNames)
+        {
+            string tableName = "FileNames";
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                int count = 2;
+                connection.Open();
+                foreach (string fileName in FileNames)
+                {
+                    string commandText = "INSERT " + tableName +
+                        " (FileName, TableNumber)" +
+                        " VALUES ('" + fileName + "', " + count + ")";
+
+                    SqlCommand cmd = new SqlCommand(commandText, connection);
+                    cmd.ExecuteNonQuery();
+                    count++;
+                }
+                connection.Close();
+            }
+        }
+
+    }
+}
---
+++ Citi_Bike_Data_01/Citi_Bike_Data_01/cls_Web_Helper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Citi_Bike_Data_01
+{
+    public class cls_Web_Helper
+    {
+        public static List<string> FileNamesLocal;
+        public static List<string> FileNamesNew;
+        public static string DestinationFolder;
+
+        /// <summary>
+        /// Retrieves the list of available citi bike datasets on the amazon s3 bucket
+        /// <reference>https://stackoverflow.com/questions/124492/c-sharp-httpwebrequest-command-to-get-directory-listing</reference>
+        /// <reference>https://stackoverflow.com/questions/7496913/how-to-load-xml-from-url-on-xmldocument</reference>
+        /// <param name="XMLWebAddress"> URL address where to locate the XML string</param>
+        /// <returns> List of strings indicating the data set names</return>
+        /// </summary>
+        public static List<string> GetListOfFileNames(string XMLWebAddress)
+        {
+            List<string> fileNamesWeb = new List<string>();                                     // will hold all the file names from the amazon site
+            string xmlStr;
+
+            using (WebClient wc = new WebClient())
+            {
+                xmlStr = wc.DownloadString(XMLWebAddress);                                      // get the xml from the website
+            }
+            XmlDocument doc = new XmlDocument();                                                // create new xml document
+            doc.LoadXml(xmlStr);                                                                // load string into document
+
+            XmlNodeList nodeList = doc.GetElementsByTagName("Key");                             // search document for KEY
+
+            foreach (XmlNode node in nodeList)                                                  // go through each node
+            {
+                if (node.InnerXml.Contains(".zip"))                                             // if the name contains a .zip
+                    fileNamesWeb.Add(node.InnerXml);                                            // save file name
+            }
+            // create a txt
+            #region OPTION 02
+            /* XmlTextReader reader = new XmlTextReader(XMLWebAddress);            //https://support.microsoft.com/en-us/help/307643/how-to-read-xml-data-from-a-url-by-using-visual-c
+             while (reader.Read())
+             {
+                 switch (reader.NodeType)
+                 {
+                     case XmlNodeType.Element:
+                         if(reader.Name == "Key")
+                         {
+                             fileNames.Add(reader.Value);
+                         }
+                         //Debug.Print( reader.Name);
+                         break;
+                     case XmlNodeType.Text:
+                        // Debug.Print(reader.Value);
+                         break;
+                     case XmlNodeType.EndElement:
+                         Debug.Print(  reader.Name);
+                         break;
+                 }
+             }*/
+            #endregion
+
+            #region OPTION 03
+            /*HttpWebRequest request = (HttpWebRequest)WebRequest.Create(WebAddress);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string html = reader.ReadToEnd();
+                    Regex regex = new Regex(WebAddress);
+                    regex = new Regex("<a href=\".*\">(?<name>.*)</a>");
+                    MatchCollection matches = regex.Matches(html);
+                    if (matches.Count > 0)
+                    {
+                        foreach (Match match in matches)
+                        {
+                            if (match.Success)
+                            {
+                                Debug.Print(match.Groups["name"].ToString());
+                            }
+                        }
+                    }
+                }
+            }*/
+            #endregion
+            return fileNamesWeb;
+        }
+        //-----------------------------------------------------------------------
+    }
+}
