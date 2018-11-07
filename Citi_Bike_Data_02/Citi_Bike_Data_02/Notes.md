b5d0061b38f0b3ee89810e1e32ebb5b2554f9c6c
crashnorun <charlie.portelli@gmail.com>
2018-11-06 20:49:44 -0500

    Started new VS project

---
+++ Citi_Bike_Data_02/Citi_Bike_Data_02/Notes.md
@@ -0,0 +1,3 @@
+﻿Create a SQL DB in VS
+https://docs.microsoft.com/en-us/visualstudio/data-tools/create-a-sql-database-by-using-a-designer?view=vs-2017
+
---
+++ Citi_Bike_Data_02/Citi_Bike_Data_02/UI/UserControl1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Citi_Bike_Data_02.UI
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_CreateDB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
