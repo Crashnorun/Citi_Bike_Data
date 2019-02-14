2899e1d525bbcb5ca2bff2e15a49260117232b8d
Crashnorun <charlie.portelli@gmail.com>
2019-02-14 08:33:29 -0500

    house cleaning

--- MapBox/main.js
+++ MapBox/main.js
@@ -15,13 +15,9 @@ function main() {
 
 function constructURL() {
     let prefix = "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/1/0/0/mvt?";
-    // let key = "crashnorun?access_token=pk.eyJ1IjoiY3Jhc2hub3J1biIsImEiOiJjajVpbnEyYXkxZ21zMzBuejIyOGRubWNmIn0.NzvTKcEi2LCV7IGScDLYJw";
     let key = access.key;
-
     let url = prefix + key;
-
-   // "https://api.mapbox.com/v4/crashnorun.id??access_token=pk.eyJ1IjoiY3Jhc2hub3J1biIsImEiOiJjajVpbnEyYXkxZ21zMzBuejIyOGRubWNmIn0.NzvTKcEi2LCV7IGScDLYJw/16.5/40.7/-73.9/.png";
-
+   
     return url;
     //@40.7410015,-73.9913487,16.5z
 }
